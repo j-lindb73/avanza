8558c3aa7c538ea5af81242e531d26ea741ac471
Johan Lindberg <johan@hasselstigen.se>
2020-10-07 16:19:31 +0100

    Added links to reports 5 and 6

--- src/components/NavReport.vue
+++ src/components/NavReport.vue
@@ -3,7 +3,9 @@
     <router-link to="/reports/week/1">Kmom01</router-link> |
     <router-link to="/reports/week/2">Kmom02</router-link> |
     <router-link to="/reports/week/3">Kmom03</router-link> |
-    <router-link to="/reports/week/4">Kmom04</router-link>
+    <router-link to="/reports/week/4">Kmom04</router-link> |
+    <router-link to="/reports/week/5">Kmom05</router-link> |
+    <router-link to="/reports/week/6">Kmom06</router-link>
     </div>
 </template>
 
